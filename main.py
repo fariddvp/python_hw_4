b69b5a4cf6d9f30560ff4ba59c2b3a847b274b00
Farid <fariddivanpour@gmail.com>
2024-03-27 16:34:58 +0330

    HW4-Part1-completed

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 ####### Home work No. 4
 ### Calculator
-
+# import madules
 from src.add import add
 from src.sub import sub
+import os
 
 
+PATH = '/home/farid/Documents/hw4/python_hw_4/src/file.txt'
+# mani Function
 def main():
     
-    input_1, input_2 = map(int,input("Enter your numbers and split them with ',' : "))
+    # inputes
+    input_1, input_2 = map(int, input("Enter your numbers and split them with ',' : ").split(','))
 
-    print(f'{input_1} + {input_2} = ' + add(input_1, input_2))
+    if not os.path.exists(PATH):
+        print('Oops! Created a file.txt in directory: ' + PATH)
+        with open(PATH, 'w+') as f:
+            pass
 
-    print(f'{input_1} + {input_2} = ' + sub(input_1, input_2))
+    with open(PATH, 'a+') as f:
+        f.write(f'{input_1} + {input_2} = {add(input_1, input_2)}\n')
+        f.write(f'{input_1} - {input_2} = {sub(input_1, input_2)}\n')
+    
+    with open(PATH, 'r') as file:
+        for line in file:
+            print(line.strip())
 
 
+    print('Your calculations saved in file.txt...')
 
 
 main()
